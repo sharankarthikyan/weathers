1a014a88d503a12ae75288fff9ce8f0b1f76e9a4
sharankarthikeyan <karthikeyansharan00@gmail.com>
2024-07-11 09:31:05 +0530

    z index issue fix

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -109,7 +109,7 @@ export default function NavBar({ toggleInput }) {
   }, []);
 
   return (
-    <div className="navbar bg-base-200 border border-neutral sticky top-0">
+    <div className="navbar bg-base-200 border border-neutral sticky top-0 z-20">
       <div className="w-[25%] justify-start">
         {/* <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
